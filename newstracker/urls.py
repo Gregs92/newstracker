98d27451693934078c686dd194c40d59f63def2c
Gregs92 <stravs.grega@gmail.com>
2013-05-20 17:09:16 +0200

    Query v vizualizaciji

--- newstracker/urls.py
+++ newstracker/urls.py
@@ -1,33 +1,26 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
 from django.conf.urls.defaults import handler404, handler500
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from view.views import error_404, error_500
 
 urlpatterns = patterns('',
-    # Examples:
-	url(r'^$', 'tracker.views.index'),
-	url(r'^analysis/', 'tracker.views.analysis'),
-	url(r'^about/', 'tracker.views.about')
-    # url(r'^newstracker/', include('newstracker.foo.urls')),
+                       url(r'^', include("view.urls")),
+                       url(r'^', include("login.urls")),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    
-)
- 
-handler500 = 'tracker.views.error_500'
-handler404 = 'tracker.views.error_404'
+handler500 = error_500
+handler404 = error_404
 
 
 #testing 404 and 500 pages
-if settings.DEBUG:
-	urlpatterns += patterns('',
-		(r'^404/$', 'django.views.defaults.page_not_found'),
-        	(r'^500/$', 'django.views.defaults.server_error'),
-	)
+'''if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^404/$', 'django.views.defaults.page_not_found'),
+        (r'^500/$', 'django.views.defaults.server_error'),
+    )'''
